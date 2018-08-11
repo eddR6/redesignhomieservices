51e8babb99eb91eba89ebf081ede5562b7b7df8b
Aditya Jain <jainaditya400@gmail.com>
2018-08-11 18:12:05 +0530

    s

---
+++ scrip.js
@@ -0,0 +1,30 @@
+
+//code to change active state of service tabs and highlight current tab
+var btnContainer= document.getElementById("services1");
+var btns = btnContainer.getElementsByClassName("btn");
+for (var i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function() {
+    var current = document.getElementsByClassName("active");
+    current[0].className = current[0].className.replace(" active", "");
+    this.className += " active";
+  });
+}
+
+
+
+
+//code to animate advertisement tab
+var myIndex = 0;
+carousel();
+
+function carousel() {
+    var i;
+    var x = document.getElementsByClassName("mySlides");
+    for (i = 0; i < x.length; i++) {
+       x[i].style.display = "none";  
+    }
+    myIndex++;
+    if (myIndex > x.length) {myIndex = 1}    
+    x[myIndex-1].style.display = "block";  
+    setTimeout(carousel, 2000); // Change image every 2 seconds
+}
